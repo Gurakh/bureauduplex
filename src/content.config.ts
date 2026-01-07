57e97202852fcc37574b327aba8e1e62d46ec292
Franck Da Silva <franck@antistatique.net>
2026-01-07 16:22:42 +0100

    Add schema for notion content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,23 @@
+import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 import { notionLoader } from "notion-astro-loader";
+import {
+  notionPageSchema,
+  transformedPropertySchema,
+} from "notion-astro-loader/schemas";
 
 const notionContent = defineCollection({
   loader: notionLoader({
     auth: import.meta.env.NOTION_TOKEN,
     database_id: import.meta.env.NOTION_DATABASE_ID,
+  }),
+  schema: notionPageSchema({
+    properties: z.object({
+      project: transformedPropertySchema.title,
+      client: transformedPropertySchema.rich_text,
+      tags: transformedPropertySchema.rich_text,
+      year: transformedPropertySchema.rich_text,
+    }),
   }),
 });
 
